ba4c4477f927d4a83bce3bfc1af1990be60cb44e
sayantanpal <sayantanpal100@gmail.com>
2025-12-25 03:38:44 +0530

    feat: Implement loading state for advice fetching and add a counter for fetched advice.

--- first-react-sample-project/src/App.js
+++ first-react-sample-project/src/App.js
@@ -1,22 +1,33 @@
 import { useState } from "react"
 export default function App() {
     const [advice,setAdvice] = useState("Fetching Advice...")
+    const [val, updatedVal] = useState(0)
+    const [isLoading, setIsLoading] = useState(false)
 let getAdvice = async() =>{
+
     try {
+          setIsLoading(true)
         let adviceFetched = await fetch("https://api.adviceslip.com/advice")
         let data = await adviceFetched.json()
         setAdvice(data.slip.advice)
+        updatedVal(prev => prev + 1)
+
         // console.log(data.slip.advice)
         
     } catch (err) {
         console.error(err.message)
+    }finally{
+        setIsLoading(false)
     }
 }
 
   return (
     <div>
       <h1>{advice}</h1>
-      <button onClick={getAdvice}>Get Advice</button>
+      <button disabled={isLoading} onClick={getAdvice}>
+        {isLoading ? "Loading..." : "Get Advice"}
+      </button>
+      <p>you have read <strong>{val}</strong> pieces of advice</p>
     </div>
   );
 }
