982b020d5c329591e09a28ecb9e490c7689fb3e4
sayantanpal <sayantanpal100@gmail.com>
2026-01-13 19:12:38 +0530

    solved Sec 4.1 HandsOn JS essentials/Sec_30_Promise_and_Async/_1_Creating_Basic_Promise_example.js

--- Sec 4.1 HandsOn JS essentials/Sec_30_Promise_and_Async/_1_Creating_Basic_Promise_example.js
+++ Sec 4.1 HandsOn JS essentials/Sec_30_Promise_and_Async/_1_Creating_Basic_Promise_example.js
@@ -22,7 +22,12 @@
 
 function createSimplePromise() {
     // ==================== YOUR CODE HERE ====================
-    
+    return new Promise((resolve, reject)=> {
+        setTimeout(() => {
+            resolve("Success!")
+            
+        }, 1000);
+    })
     
     
     // ========================================================
@@ -46,7 +51,14 @@ function createSimplePromise() {
 
 function checkNumber(num) {
     // ==================== YOUR CODE HERE ====================
-    
+    return new Promise((resolve, reject)=> {
+        if(num>10){
+            resolve("Number is greater than 10")
+        }
+        else{
+            reject("Number is 10 or less")
+        }
+    })
     
     
     // ========================================================
@@ -67,7 +79,11 @@ function checkNumber(num) {
 
 function delayedValue(value, delay) {
     // ==================== YOUR CODE HERE ====================
-    
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            resolve(value)
+        }, delay);
+    })
     
     
     // ========================================================
