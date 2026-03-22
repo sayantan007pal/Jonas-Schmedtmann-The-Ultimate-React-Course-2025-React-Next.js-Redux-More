7598ab169627df7f493a9e1090b3f14774f176b4
sayantanpal <sayantanpal100@gmail.com>
2026-03-22 16:41:32 +0530

    refactor: update package dependencies and restructure App component layout

--- Sec 6 State, Events and Forms : Interactive Components/steps/src/App.js
+++ Sec 6 State, Events and Forms : Interactive Components/steps/src/App.js
@@ -4,7 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-          Hello World
+          <div className='numbers'>
+            <div >1</div>
+            <div >2</div>
+            <div >3</div>
+         </div>
+         <div className='message'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaque.</p>
+         </div>
+         <div className='buttons'>
+            <button>
+              <span>Previous</span>
+            </button>
+            <button>
+              <span>Next</span>
+            </button>
+         </div>
+        
+
     </div>
   );
 }
