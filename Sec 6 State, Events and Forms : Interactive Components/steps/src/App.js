4982a6fed211f86010d7640c85932a148b809d81
sayantanpal <sayantanpal100@gmail.com>
2026-03-22 17:25:05 +0530

    refactor: enhance step display logic and update message rendering in App component

--- Sec 6 State, Events and Forms : Interactive Components/steps/src/App.js
+++ Sec 6 State, Events and Forms : Interactive Components/steps/src/App.js
@@ -1,22 +1,29 @@
 
 import './App.css';
 
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 function App() {
+  const step = 2;
   return (
     <div className="App">
           <div className='numbers'>
-            <div >1</div>
-            <div >2</div>
-            <div >3</div>
+            <div className= {`${step >= 1 ? 'active' : ''}`}>1</div>
+            <div className= {`${step >= 2 ? 'active' : ''}`}>2</div>
+            <div className= {`${step === 3 ? 'active' : ''}`}>3</div>
          </div>
          <div className='message'>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaque.</p>
+            <p>Step: {step} {messages[step - 1]}</p>
          </div>
          <div className='buttons'>
-            <button>
+            <button style={{backgroundColor: "#7950f2", color: "#fff"}} className='button active'>
               <span>Previous</span>
             </button>
-            <button>
+            <button style={{backgroundColor: "#7950f2", color: "#fff"}} className='button active'>
               <span>Next</span>
             </button>
          </div>
